5a2ffe06f9133327e67803eb3f1340fe91b46d5d
Isha <ishaguptait@gmail.com>
2018-03-29 22:10:19 +1100

    [FEAT] some refactoring

--- app/src/main/java/test/learn/com/demo/ui/MainActivity.java
+++ app/src/main/java/test/learn/com/demo/ui/MainActivity.java
@@ -3,7 +3,6 @@ package test.learn.com.demo.ui;
 import android.demo.com.demo.R;
 
 import test.learn.com.demo.data.model.ApiResponse;
-import test.learn.com.demo.data.model.Row;
 
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -11,39 +10,43 @@ import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-
-import java.util.List;
+import android.support.v7.widget.Toolbar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements MainView{
+public class MainActivity extends AppCompatActivity implements MainView {
 
     @BindView(R.id.recyclerView)
     protected RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
-    private RecyclerAdapter adapter;
-    private List<Row> responses;
-    private android.support.v7.widget.Toolbar mTopToolbar;
+    private RecyclerAdapter recyclerAdapter;
+    private Toolbar mTopToolbar;
     @BindView(R.id.pullToRefresh)
     SwipeRefreshLayout pullToRefresh;
-    MainPresenter<MainView> presenter ;
+    MainPresenter<MainView> presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initUI();
+        fetchDataFromApi();
+    }
+
+    private void fetchDataFromApi() {
+        presenter = new MainPresenter<MainView>(MainActivity.this);
+        presenter.onAttach(this);
+        presenter.requestData();
+    }
+
+    private void initUI() {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
         mTopToolbar = findViewById(R.id.toolbar);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-
         pullToRefresh.setRefreshing(false);
-        presenter=new MainPresenter<MainView>(MainActivity.this);
-        presenter.onAttach(this);
-
-        presenter.requestData();
 
         pullToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
@@ -62,13 +65,11 @@ public class MainActivity extends AppCompatActivity implements MainView{
 
     @Override
     public void updateViewWithData(ApiResponse apiResponse) {
-
-        pullToRefresh.setRefreshing(false);
         mTopToolbar.setTitle(apiResponse.getTitle());
+        pullToRefresh.setRefreshing(false);
 
-        responses = apiResponse.getRows();
-        adapter = new RecyclerAdapter(responses, MainActivity.this);
-        recyclerView.setAdapter(adapter);
+        recyclerAdapter = new RecyclerAdapter(apiResponse.getRows(), MainActivity.this);
+        recyclerView.setAdapter(recyclerAdapter);
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), 1);
         recyclerView.addItemDecoration(dividerItemDecoration);
     }
